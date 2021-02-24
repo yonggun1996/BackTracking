ee5b1c1e5c576427d44344a84e0bbba60af63f49
qkrdydrjs7@naver.com <qkrdydrjs7@naver.com>
2021-02-25 00:51:47 +0900

    백준 2580번 문제입니다.

--- BackTracking/src/No2580.java
+++ BackTracking/src/No2580.java
@@ -1,44 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-//import java.util.Arrays;
-//import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class No2580 {
 	
 	static int[][] sdokoo_array = new int[9][9];
 	
-	public static void main(String[] args) throws IOException {
-		//Scanner in = new Scanner(System.in);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
 		for(int i = 0; i < 9; i++) {
 			for(int j = 0; j < 9; j++) {
-				st = new StringTokenizer(br.readLine());
-				sdokoo_array[i][j] = Integer.parseInt(st.nextToken());
+				sdokoo_array[i][j] = in.nextInt();
 			}
 		}
 		
 		search(0, 0);
 	}
 	
-	public static void search(int x, int y) throws IOException {
+	public static void search(int x, int y){
 		if(x == 9 || y == 9) {
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			
 			for(int i = 0; i < 9; i++) {
 				for(int j = 0; j < 9; j++) {
-					bw.write(String.valueOf(sdokoo_array[i][j]) + " ");
 					//System.out.print(sdokoo_array[i][j] + " ");
 				}
-				bw.newLine();
 			}
 			
-			bw.flush();
 			System.exit(0);
 		}else {
 			if(sdokoo_array[x][y] == 0) {
